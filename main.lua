b1ed058c41f426f00ad9d7d5aa71d82cc80b3d11
M4rk1122 <52112019+M4rk1122@users.noreply.github.com>
2023-06-30 18:07:00 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -23,18 +23,6 @@ RegisterNuiCallback('close', function()
     StopVideo()
 end)
 
-RegisterCommand('PlayVideo', function(source, args)
-    PlayVideo(args[1])
-end, false)
-
-RegisterCommand('StopVideo', function(source, args)
-    StopVideo((args[1] ~= nil))
-end, false)
-
-RegisterCommand('SetNuiDisplay', function(source, args)
-    SetNuiDisplay((args[1] == 'on') and true or false)
-end, false)
-
 exports('PlayVideo', function(...)
     PlayVideo(...)
 end)
